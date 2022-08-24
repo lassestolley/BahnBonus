0797c8e479ad26d77a2b49d5ff40f6797491e510
Lasse Stolley <me@lassestolley.com>
2022-08-24 19:22:12 +0200

    Initial Commit

---
+++ BahnBonus/BahnBonusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BahnBonusApp.swift
+//  BahnBonus
+//
+//  Created by Lasse Stolley on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct BahnBonusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BahnBonus/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BahnBonus
+//
+//  Created by Lasse Stolley on 24.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
